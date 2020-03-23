8f81de4ca57b431eecc2642240d924cf1129a4cf
itGabriel <gabriel.gustavo.s@hotmail.com>
2020-03-23 19:44:16 -0300

    styling

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, escape, render_template, request, redirect, session, fl
 
 app = Flask(__name__)
 
-access_token = "SEU TOKEN"
+access_token = "24aa25e471711f0f8cb1a69dc7daf216"
 
 cliente = Cliente(access_token, 'https://api2.totalvoice.com.br')
 
@@ -18,6 +18,17 @@ def index():
     return render_template('index.html', alerta = None)
 
 
+# #Cria sms
+@app.route('/sms/enviar_sms/', methods=['POST'])
+def enviar_sms():
+    numero_destino = request.form['numero_destino']
+    mensagem = request.form['sms_txt']
+    print(f"Número -> {numero_destino} \n\nMensagem -> {mensagem}")
+    response = cliente.sms.enviar(numero_destino, mensagem)
+    # print(response)
+    return render_template('index.html', alerta = f"Teste SMS - mensagem enviada para o número: {numero_destino}")
+
+
 # TTS
 # Módulo responsável por criação de Audios.
 @app.route('/tts/enviar/', methods=['POST'])
@@ -27,31 +38,6 @@ def enviar_mensagem_deaudio():
     response = cliente.tts.enviar(numero_destino, mensagem)
     print(response)
     return render_template('index.html', alerta = f"Teste TTS - mensagem enviada para o número: {numero_destino}")
-
-
-#Cria chamada
-# @app.route('/ligacao/chamar/<numero_origem>/<numero_destino>')
-# def realizar_chamada(numero_origem, numero_destino):
-#     response = cliente.chamada.enviar(numero_origem, numero_destino)
-#     print(response)
-#     return "Teste de 'ligação' executado"
-
-
-# #Cria sms
-# @app.route('/sms/enviar_msmsimples/<numero_destino>/<mensagem>')
-# def enviar_sms(numero_destino, mensagem):
-#     response = cliente.sms.enviar(numero_destino, mensagem)
-#     print(response)
-#     return "Teste de 'SMS enviado' executado"
-
-
-
-# @app.route('/validacao/numero/<numero_avalidar>')
-# #cria um registro validaNumero que irá checar se o número é válido
-# def verifica_numero(numero_avalidar):
-#     response = cliente.valida_numero.criar(numero_avalidar)
-#     print(response)
-#     return "Teste de 'validação de número' executado"
 
 
 if __name__ == "__main__":
